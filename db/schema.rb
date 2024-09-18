3f66393fb2b3170b81dc81141fa301bc29e988ec
L F <franco100lucas@gmail.com>
2024-09-17 21:07:58 -0300

    feat: Add category to permit have many products and update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_11_005720) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_17_232344) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -46,6 +46,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_11_005720) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
@@ -61,5 +63,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_11_005720) do
   add_foreign_key "order_items", "orders"
   add_foreign_key "order_items", "products"
   add_foreign_key "orders", "users"
+  add_foreign_key "products", "categories"
   add_foreign_key "products", "users"
 end
