2544c266a9b30dea4fc6a25a276c3e5c806ce311
L F <franco100lucas@gmail.com>
2024-09-20 09:19:26 -0300

    fix: Adding and change some user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,9 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  # validates :email, presence: true, uniqueness: true
-  # validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :username, uniqueness: true #, presence: true
+  validates :username, presence: true, length: { in: 3..40 } #, uniqueness: false
+  validates :email, uniqueness: true, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { in: 6..20 }
+  validates :phone_number, presence: true, length: { is: 11 }, numericality: { only_integer: true }
 
 end
