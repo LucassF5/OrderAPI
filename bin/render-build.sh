9c2faa51e72dbe9731811007bbd1ff16d7833d62
L F <franco100lucas@gmail.com>
2024-09-24 15:41:06 -0300

    chore: Update render-build.sh script and add database migration task

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,14 @@
+# #!/usr/bin/env bash
+# set -o errexit
+
+# bundle install
+# bundle exec rails assets:precompile
+# bundle exec rails assets:clean
+
+
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
+bundle exec rake db:migrate
