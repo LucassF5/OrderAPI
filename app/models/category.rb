dcad8b4c2f91470036ca09af5a3101b2b2322796
L F <franco100lucas@gmail.com>
2024-09-27 17:52:37 -0300

    Fix: Updating files to use Store table

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
-  belongs_to :user
+  belongs_to :store
   has_many :products
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
-  has_many :products, dependent: :destroy
   has_many :orders, dependent: :destroy
-  has_many :categories, dependent: :destroy
+  has_one :store, dependent: :destroy
 
   has_secure_password
 
+  
   # validates :username, presence: true, length: { in: 3..40 } #, uniqueness: false
   validates :email, uniqueness: true, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password, presence: true, length: { in: 6..20 }
+  validates :password, presence: true, length: { in: 6..20 }, on: :create
   # validates :phone_number, presence: true, length: { is: 11 }, numericality: { only_integer: true }
-
+  # validates :isLoja, inclusion: { in: [true, false] }
 end
